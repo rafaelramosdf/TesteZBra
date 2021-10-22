bf2c91838d5ffa2c6a762c5a8a5b06be9c350a11
Rafael Ramos <rafaelramosdf@live.com>
2021-10-22 00:55:30 -0300

    Implementada questao dois parte um

---
+++ TesteZBra.QuestaoDois.ParteUm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace TesteZBra.QuestaoDois.ParteUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaIDs = ListaID.IDs.Split('-').ToList();
+            var quantidadeIdsLetrasDuplas = listaIDs.Count(id => id.GroupBy(g => g).Any(g => g.Count() == 2));
+            var quantidadeIdsLetrasTriplas = listaIDs.Count(id => id.GroupBy(g => g).Any(g => g.Count() == 3));
+            var somaVerificacao = quantidadeIdsLetrasDuplas * quantidadeIdsLetrasTriplas;
+
+            Console.WriteLine($"A soma de verificação é: {somaVerificacao}");
+            Console.ReadLine();
+        }
+    }
+}
